fba8b26986f8a0145191cc88b9c80c0476fcbc56
harshgarg10 <garg48301@gmail.com>
2025-07-19 22:07:03 +0530

    fixed no connecction error

--- server/config/db.js
+++ server/config/db.js
@@ -6,16 +6,16 @@ console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_PASS:', process.env.DB_PASS ? 'Password exists' : 'No password');
 console.log('DB_NAME:', process.env.DB_NAME);
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'codearena',
+onst pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
-});
-
+  queueLimit: 0
+}).promise();
 // Test the connection
 pool.getConnection()
   .then(connection => {
